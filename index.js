53859b98c494b3117c462eb49ef05b1a19158c2a
Parker Williamson <parker.williamson@gmail.com>
2020-08-21 20:22:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+class Dog {
+  constructor(name, sex) {
+    this.name = name; 
+    this.sex = sex;
+  } 
+  speak() {
+    return `${this.name} says woof!`
+  }
+}
+
+class Cat {
+    constructor(name, sex) {
+    this.name = name; 
+    this.sex = sex;
+  } 
+  speak() {
+    return `${this.name} says meow!`
+  }
+}
+
+class Bird {
+  constructor(name, sex) {
+    this.name = name; 
+    this.sex = sex;
+  } 
+  speak() {
+    return `${this.name} says squawk!`
+  }
+}
